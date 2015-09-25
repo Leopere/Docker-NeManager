5525638cf8161c463207c5f03d1ae905fb01b8fa
chamunks <nixium.net@gmail.com>
2015-09-25 16:38:08 -0400

    Added launch script

--- nema.sh
+++ nema.sh
@@ -4,9 +4,17 @@ case $1 in
     wget http://tcpr.ca/files/paperspigot/PaperSpigot-1.8.8-R0.1-SNAPSHOT-latest.jar -O ./jars/PaperSpigot-1.8.8-R0.1-SNAPSHOT-latest.jar
     wget http://tcpr.ca/files/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar -O ./jars/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar
     ;;
+  run )
+  docker run -ti \
+      --name nema \
+      -v "./data:/data" \
+      -v "./jars:/jars" \
+      -v "./servers:/servers" \
+      -p "20000:20000" \
+      -p "25565-25629:25565-25629" \
+      chamunks/docker-nemanager-wrapper
+    ;;
   * )
-    print `running "./getjars.sh latest" will install a spigot and paperspigot`
-    print `jar into your wrappers jars directory.`
-    print `This may get out of date so don't rely on it.`
+    echo "your options are run and latest"
     ;;
 esac
